df42085cb6578fce3850871a23ca57195e532c82
The Landlocked Albatross <thelandlockedalbatross@proton.me>
2026-09-13 11:41:28 -0400

    Repoint Super+Ctrl+A to omarchy 4 shell audio panel; delete dead omarchy-launch-audio (wiremix) shim and all callers

--- .config/omarchy/extensions/menu.sh
+++ .config/omarchy/extensions/menu.sh
@@ -166,7 +166,7 @@ show_setup_menu() {
   options="$options\n󰄰  Auto-focus\n󰱔  DNS\n󰕥  VPN\n󰒃  Security\n󰒓  Config"
 
   case $(menu "Setup" "$options") in
-  *Audio*) omarchy-launch-audio ;;
+  *Audio*) omarchy-shell shell toggle omarchy.audio ;;
   *Wifi*) omarchy-shell shell toggle omarchy.network ;;
   *Bluetooth*) omarchy-shell shell toggle omarchy.bluetooth ;;
   *Power*) show_setup_power_menu ;;
